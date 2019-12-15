d3f181b1b34d7d2e4b428636f8f4ce5c42724fe3
Anton Chuchva <56050552+DeadLauncher@users.noreply.github.com>
2019-12-16 01:15:26 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -6,8 +6,8 @@ def space_remove(string):
         string = string[1:]
     return string
 class serial:
-    #def __init__(self, tittle,original,desc, genres_ids, actors_ids,author_id,status,channel_id,thumb,timing,rating):
-        '''
+    '''
+    def __init__(self, tittle,original,desc, genres_ids, actors_ids,author_id,status,channel_id,thumb,timing,rating):  
         self.tittle = tittle
         self.original = original
         self.desc = desc
@@ -20,14 +20,16 @@ class serial:
         self.timing = timing
         self.rating = rating
         '''
-
+        
 class season:
+    '''
     def __init__(id,serial_id,tittle):
         self.id = id
         self.serial_id = serial_id
         self.tittle = tittle
-
+    '''
 class episode:
+    '''
     def __init__(id,season_id,number,tittle,original,date):
         self.id = id
         self.season_id = season_id
@@ -35,7 +37,7 @@ class episode:
         self.tittle = tittle
         self.original = original
         self.date = date
-
+    '''
 if __name__ == "__main__":
     serials = []
     seasons = []
@@ -52,6 +54,7 @@ if __name__ == "__main__":
         soup = BeautifulSoup(request,'lxml')
         header = soup.find('h1', {'class': 'title-basic'})
         if header == None:
+            i -= 1
             continue
         new_serial = serial()
         new_serial.tittle = soup.find('span', {'itemprop': 'name'}).text
@@ -67,18 +70,17 @@ if __name__ == "__main__":
                     genres.append(content[k].text)
                 new_serial.genres_ids.append(genres.index(content[k].text)+1)
         new_serial.desc = soup.find('p',{'class':'body_large summary'}).text
+        new_serial.id = len(serials)+1
         serials.append(new_serial)
         header = BeautifulSoup(str(soup.find('div',{'class':'content-widget-1'})),'lxml')
         content = header.div.contents
         info2 = BeautifulSoup(str(header.find('a')),'lxml')
-        print(info2.a)
         if info2.a == None:
             new_serial.authors_ids = [1]
             new_serial.actors_ids = [1]
         else:
             new_serial.authors_ids = []
             new_serial.actors_ids = []
-            print("https://www.toramp.com/"+info2.a.attrs['href'])
             html2 = requests.get("https://www.toramp.com/"+info2.a.attrs['href'])
             soup1 = BeautifulSoup(html2.text,'lxml')
             blocks = soup1.findAll('div',{'class':'block_list'},'lxml')
@@ -93,15 +95,44 @@ if __name__ == "__main__":
                     actors.append(j)
                 new_serial.actors_ids.append(actors.index(j)+1)
         new_serial.channel_id = content[9].text
-        if content[9] not in channels:
+        if content[9].text not in channels:
             channels.append(content[9].text)
         new_serial.channel_id = channels.index(content[9].text)+1
-        if content[5] not in statuses:
+        if content[5].text not in statuses:
             statuses.append(content[5].text)
         new_serial.status_id = statuses.index(content[5].text)+1
         new_serial.rating = soup.find('div',{'id':'ratStat'}).text
         header = BeautifulSoup(str(soup.find('td',{'id':'img_basic'})),'lxml')
         new_serial.thumb = header.td.img.attrs['src']
+
+        header = BeautifulSoup(str(''.join(list(map(str,soup.findAll('div',{'id':'full-season'}))))),'lxml')
+        s = header.div
+        #l = len(seasons) + int(header.find('a',{'class' : 'numbering'}).text.split('x')[1])
+        while True:
+            new_season = season()
+            new_season.serial_id = len(serials)
+            new_season.tittle = s.h2.text
+            seasons.append(new_season)
+            #print(s.h2.text)
+            for n in s.findAll('tr'):
+                new_episode = episode()
+                new_episode.season_id = len(seasons)
+                new_episode.number = n.a.span.text
+                #print(n.a.span.text)
+                new_episode.tittle = n.td.nextSibling.b.text
+                #print('  ',n.td.nextSibling.b.text)
+                if n.td.nextSibling.span == None:
+                    new_episode.original = ''
+                    #print('  ','')
+                else:
+                    new_episode.original = n.td.nextSibling.span.text
+                    #print('  ',n.td.nextSibling.span.text)
+                new_episode.published = n.td.nextSibling.nextSibling.nextSibling.span.text
+                #print('  ',n.td.nextSibling.nextSibling.nextSibling.span.text)
+                episodes.append(new_episode)
+            s = s.nextSibling
+            if s == None:
+                break
         print(i,'/',count)
     for i in range(len(genres)):
         print(i+1,genres[i])
@@ -114,16 +145,35 @@ if __name__ == "__main__":
     for i in range(len(actors)):
         print(i+1,actors[i])
     for i in serials:
+        print()
         print(i.tittle,"(",i.original,")")
-        print('Длительность:',i.timing)
-        print('Жанры:',i.genres_ids)
-        print(i.desc)
-        print("Статус:",statuses[i.status_id-1])
-        print(i.channel_id)
-        print(i.rating)
         print(i.thumb)
+        print('Длительность:',i.timing)
+        print('Жанры:')
+        for j in i.genres_ids:
+            print(genres[j-1])
+        print('Описание:')
+        print(i.desc)
+        print('Статус:',statuses[i.status_id-1])
+        print('Канал:',channels[i.channel_id-1])
+        print('Рейтинг:',i.rating)
         print("Авторы:")
-        print(i.authors_ids)
+        for j in i.authors_ids:
+            print(authors[j-1])
         print("Актёры:")
-        print(i.actors_ids)
+        for j in i.actors_ids:
+            print(actors[j-1])
+    for i in seasons:
+        print(seasons.index(i)+1)
+        print(i.tittle)
+        print("Сериал: ",serials[i.serial_id-1].tittle)
+    for i in episodes:
+        print(episodes.index(i)+1)
+        print(i.tittle)
+        print(i.original)
+        print("Сезон: ",seasons[i.season_id-1].tittle,'('+serials[seasons[i.season_id-1].serial_id-1].tittle+')')
         
+        #parse time:        1.58 min
+        #full time:         2.34 min 
+        #serials parsed:    100 (with empty pages)
+        #episodes parsed:   12751
